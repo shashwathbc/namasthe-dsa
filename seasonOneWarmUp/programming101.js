1e51906a7cb82748895454729a7b83252b576707
shashwathbc <shashwathbc@gmail.com>
2025-07-04 01:41:38 +0530

    [Namasthe Dev]: prograsmming 101 completed

--- seasonOneWarmUp/programming101.js
+++ seasonOneWarmUp/programming101.js
@@ -1,1 +1,47 @@
 //
+console.log('Hellow world');
+
+// anything you write inside single or double quotes is a string
+console.log("Hellow world");
+
+// anything you write inside backticks is a template literal
+console.log(`Hellow world`);
+
+// variables are used to store data
+let name = "Namasthe DSA";  
+
+// variables can be declared using let, const, or var
+// let is used to declare a variable that can be changed later
+// const is used to declare a variable that cannot be changed later
+// var is used to declare a variable that can be changed later
+console.log(name);
+// variables can be used to store different types of data
+let isStudent = true; // boolean data type
+let favoriteColor = "blue"; // string data type 
+let favoriteNumber = 7; // number data type 
+
+
+
+// 
+
+const a = 10;
+const b = 20;
+
+// arithmetic operations
+const sum = a + b;
+console.log(`The sum of ${a} and ${b} is ${sum}`); // using template literals to print the sum
+
+const multiply = a * b;
+console.log(`The product of ${a} and ${b} is ${multiply}`); 
+
+const difference = a    - b;
+console.log(`The difference between ${a} and ${b} is ${difference}`);
+
+const arr = [2,3,4,5,100,34];
+console.log(arr);
+
+// javascript stores this in an index and value pair, index and value pair is called an array   
+
+
+
+
