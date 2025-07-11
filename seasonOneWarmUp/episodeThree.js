4950e83b8ab30e0e3c48f26048426b70c54a2dd5
shashwathbc <shashwathbc@gmail.com>
2025-07-12 00:20:17 +0530

    Namasthe Dev: function that returns the number of negative number in an array

--- seasonOneWarmUp/episodeThree.js
+++ seasonOneWarmUp/episodeThree.js
@@ -1,5 +1,7 @@
 // Loop - 02
 
+
+//Q1 : write a function that searches for an eklement in an array and returns its index if found or -1 if not found
 let arr = [1,2,3,4,5];
 
 function search(n){
@@ -13,3 +15,25 @@ function search(n){
 
 console.log(search(5));
 console.log(search(6));
+
+
+//Q2: write a function that returns the number of negative number in an array
+
+let arr2 = [1,-2,3,-4,0,-5];
+let positiveNumber = [];
+let negativeNumber = [];
+
+function countNegativeNumberInAnArray(arr){
+   let count = 0;
+   for(let i=0;i<arr.length; i++){
+    if(arr[i] > 0){
+        positiveNumber.push(arr[i]);
+    }else if(arr[i] < 0){
+        negativeNumber.push(arr[i]);
+        count = negativeNumber.length;
+    }
+   }
+   return `Total negative number in an array is ${count}`;
+}
+
+console.log(countNegativeNumberInAnArray(arr2));
