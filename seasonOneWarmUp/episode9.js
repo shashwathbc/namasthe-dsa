728be2fd4319b90e331c4cf6e8d6013999510fa8
shashwathbc <shashwathbc@gmail.com>
2025-07-17 22:44:39 +0530

    Namsthe dev : Reverse Integer

--- seasonOneWarmUp/episode9.js
+++ seasonOneWarmUp/episode9.js
@@ -1,10 +1,29 @@
 // reverse integer 
 
 
-function reverseInteger(n){
+function reverseInteger(num){
  
+    let nCopy = num;
+    let rev = 0;
+    num = Math.abs(num);
+
+    while(num > 0){
+        let rem = num % 10;
+        rev = (10 * rev) + rem;
+        num = Math.floor(num / 10);
+    }
+
+    let limit = Math.pow(2,31);
+
+    if(rev < -limit || rev > limit) return console.log(0);
+    
+
+    console.log(nCopy < 0 ? -rev : rev);
+
 }
+
+
 
 // reverseInteger(123);
 // reverseInteger(123454);
-reverseInteger(-123);
+reverseInteger(12987987987987987987987897980); 
