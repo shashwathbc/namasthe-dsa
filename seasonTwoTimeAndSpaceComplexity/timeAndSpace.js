db44fffa00bb6aaf8c8c211322b1cb853c1cb0e8
shashwathbc <shashwathbc@gmail.com>
2025-07-17 23:07:17 +0530

    Namsthe Dev : Time complexity

---
+++ seasonTwoTimeAndSpaceComplexity/timeAndSpace.js
@@ -0,0 +1,7 @@
+// Time and space complexity 
+
+// Algorithem : Solution which you are developing for a problem is called algorithem 
+
+// Efficiancy of the algorithem is called time and space complexity
+
+// Time complexity : It is used to mesure efficancy of the algorithem in terms of speed , as the input size grows.
