bd0733da947b1dff544db9ab2f290c940ccb9528
shashwathbc <shashwathbc@gmail.com>
2025-08-03 21:16:10 +0530

    Namasthedev: Reverse string

---
+++ season3Arrays/episode3.js
@@ -0,0 +1,25 @@
+// reverse string
+
+// function reverseString(s){
+//    let reverse = [];
+//    reverse = s.reverse();
+//    console.log(reverse);
+// }
+
+// reverseString(['s','h','a','s','h'])
+
+
+function reverseString(s){
+   // run a half loop start i with 0 till n/2
+   let len = s.length;
+    let halfLen = Math.floor(len/2);
+
+    for(let i =0; i< halfLen; i++){
+        let temp = s[i];
+        s[i] = s[len -1-i];
+        s[len -1-i] = temp;
+    }
+   
+}
+
+reverseString(['s','h','a','s','h'])
