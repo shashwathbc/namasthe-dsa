f459a2d76fe5ee7058938c8d6d2ba27a657dbbf7
shashwathbc <shashwathbc@gmail.com>
2025-08-06 00:49:56 +0530

    test

--- season3Arrays/episode4.js
+++ season3Arrays/episode4.js
@@ -1,19 +1,19 @@
 // shares profit
 
-function maxProfit(prices) {
-    let maxProfit = 0;
+// function maxProfit(prices) {
+//     let maxProfit = 0;
 
-    for (let i = 0; i < prices.length; i++) {
-        for (let j = i + 1; j < prices.length; j++) {
-            const profit = prices[j] - prices[i];
-            if (profit > maxProfit) {
-                maxProfit = profit;
-            }
-        }
-    }
+//     for (let i = 0; i < prices.length; i++) {
+//         for (let j = i + 1; j < prices.length; j++) {
+//             const profit = prices[j] - prices[i];
+//             if (profit > maxProfit) {
+//                 maxProfit = profit;
+//             }
+//         }
+//     }
 
-    return maxProfit;
-}
+//     return maxProfit;
+// }
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5 (buy at 1, sell at 6)
-console.log(maxProfit([7, 6, 4, 3, 1]));    // Output: 0 (no profit)
+// console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5 (buy at 1, sell at 6)
+// console.log(maxProfit([7, 6, 4, 3, 1]));    // Output: 0 (no profit)
