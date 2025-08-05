3c0127b41c719b86ce5dffcf1f680ee5c38daf2d
shashwathbc <shashwathbc@gmail.com>
2025-08-06 01:59:28 +0530

    Namasthedev: fbuy sell shares

--- season3Arrays/episode4.js
+++ season3Arrays/episode4.js
@@ -17,3 +17,39 @@
 
 // console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5 (buy at 1, sell at 6)
 // console.log(maxProfit([7, 6, 4, 3, 1]));    // Output: 0 (no profit)
+
+
+// function minMax(p){
+//   let max = p[0];
+//   let min = p[0];
+
+//   for(let i=0; i<p.length; i++ ){
+//     if(p[i] < min){
+//         min = p[i];
+//     }
+//     if(p[i] > max){
+//         max = p[i];
+//     }
+//   }
+//   return console.log(min, max, 'max: ', max-min);
+// };
+
+// minMax([7,1,5,3,6,4]);
+
+//  let minPrice = Infinity;
+//     let maxProfit = 0;
+
+//     for (let i = 0; i < prices.length; i++) {
+//         // Update the minimum price so far
+//         if (prices[i] < minPrice) {
+//             minPrice = prices[i];
+//         } 
+//         // Calculate profit if sold today and update maxProfit
+//         else if (prices[i] - minPrice > maxProfit) {
+//             maxProfit = prices[i] - minPrice;
+//         }
+//     }
+
+//     return maxProfit;
+
+// }
