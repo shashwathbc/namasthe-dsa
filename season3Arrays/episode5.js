8697fbe697e427e0522f9145670d989869d47330
shashwathbc <shashwathbc@gmail.com>
2025-08-07 16:59:34 +0530

    merge sorted array

--- season3Arrays/episode5.js
+++ season3Arrays/episode5.js
@@ -29,3 +29,18 @@ let i =0, j=0, l = m+n;
 }
 
 merge([1,2,3,0,0,0],[2,5,6], 3, 3);
+
+
+ let nums1Copy = nums1.slice(0,m);
+   let p1 = 0;
+   let p2 = 0;
+
+   for(let i =0;i<m+n;i++){
+    if(p2 >= n || ( p1 < m && nums1Copy[p1] < nums2[p2])){
+        nums1[i] = nums1Copy[p1];
+        p1++;
+    }else{
+        nums1[i] = nums2[p2];
+        p2++;
+    }
+   }
