a878b24179a19eef372d4aa317a6e5753ecd7d20
shashwathbc <shashwathbc@gmail.com>
2025-08-09 03:18:37 +0530

    move zeros

--- season3Arrays/episode6.js
+++ season3Arrays/episode6.js
@@ -1,1 +1,24 @@
 // move zeros:
+
+function movezero(arr){
+
+  let noOfZero = [];
+  let arr2= [];
+  for(let i=0;i<arr.length;i++){
+    // if(arr[i] === 0){
+    //   noOfZero[i] = arr[i];
+    // }else{
+    //   arr2[i] = arr[i];
+    // }
+     if (arr[i] === 0) {
+      noOfZero.push(arr[i]);
+    } else {
+      arr2.push(arr[i]);
+    }
+  }
+   const result = arr2.concat(noOfZero);
+  console.log(result);
+  return result;
+}
+
+movezero([0,1,0,3,12]);
