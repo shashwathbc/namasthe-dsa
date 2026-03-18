d1fd61d7b5c9d37213aaceb402de7c34d2fa12fc
shashwathbc <shashwathbc@gmail.com>
2026-03-18 10:23:40 +0530

    recursion episone 1 finished

--- season4Recursion/episode1.js
+++ season4Recursion/episode1.js
@@ -1,8 +1,26 @@
 // Function that calls itself to solve smaller versions of the same problem is called recursion
 
-function recusrion(){
-    console.log('Namasthe DSA');
-    recusrion();
-}
+// function recusrion(){
+//     console.log('Namasthe DSA');
+//     recusrion();
+// }
 
-recusrion();
+// recusrion();
+
+ 
+// const  printDecending = (n) => {
+//   if (n ===0) return;
+//   console.log(n);
+//   printDecending(n-1);
+// } 
+// printDecending(5);
+
+
+// print 1 to n 
+
+const print = (n, value = 1) => {
+    if(value > n) return;
+    console.log(value);
+    value = print(n, value +1);
+}
+print(5);
---
+++ season4Recursion/episode2.js
@@ -0,0 +1,2 @@
+// SUM OF FIRST N NUMBERS 
+
